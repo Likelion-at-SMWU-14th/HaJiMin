013bd5d958078cf7ea76a1416393db46b543a706
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-04-04 17:40:40 +0900

    Feat: 로그인 기능 추가

--- week04_hw/accounts/views.py
+++ week04_hw/accounts/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from django.contrib.auth import authenticate, login as auth_login
 
 def signup(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
-        print("POST 요청 옴")
-        print(form.errors)
+        print(form.errors) # 에러 확인 
         if form.is_valid():
             form.save()
-            return redirect("login")
+            return redirect("login") # 로그인으로 이동
     else:
         form = SignupForm()
 
-    return render(request, "signup.html", {"form":form})
+    return render(request, "signup.html", {"form":form}) # 회원가입 화면 렌더링
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password) # 사용자 인증
+        print(f"user: {user}") # 사용자 확인
+        if user is not None: # 존재하는 사용자인 경우
+            auth_login(request, user) # 장고 기본 로그인 메소드 
+            return redirect("main") # main으로 이동
+    
+    return render(request, "login.html") # 로그인 화면 렌더링
