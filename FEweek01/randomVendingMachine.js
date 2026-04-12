616ed6441e142ad8f5d870fe49cd3157c760436f
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-04-12 16:18:00 +0900

    feat: indexHW.html 생성 및 데이터 설정

---
+++ FEweek01/randomVendingMachine.js
@@ -0,0 +1,25 @@
+// 데이터 설정
+const fanta = {
+    name: "환타",
+    price: 2000
+};
+const coke = {
+    name: "콜라",
+    price: 1800
+};
+const sprite = {
+    name: "사이다",
+    price: 2500
+};
+const milkis = {
+    name: "밀키스",
+    price: 2800
+};
+const drPepper = {
+    name: "닥터페퍼",
+    price: 3000
+};
+
+const drinks = [fanta, coke, sprite, milkis, drPepper];
+let balance = 6500;
+
