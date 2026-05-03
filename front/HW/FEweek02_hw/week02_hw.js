38f772835bd8fdef49a11a8b582b2e8ed16bc644
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-05-03 23:25:58 +0900

    feat: 좋아요 기능 구현

--- front/HW/FEweek02_hw/week02_hw.js
+++ front/HW/FEweek02_hw/week02_hw.js
@@ -1,7 +1,28 @@
 // DOM 요소 선택
 const likeBtn = document.querySelector("like-btn");
+const likeText = document.querySelector("like-text");
 const bookmarkBtn = document.querySelector("bookmark-btn");
 const moreBtn = document.querySelector("more-btn");
 const lessBtn = document.querySelector("less-btn");
+
+// 좋아요 버튼 클릭
+likeBtn.addEventListener("click", function() {
+    likeBtn.classList.toggle("clicked");
+
+    // 현재 좋아요 수 
+    const currentLikeNum = parseInt(likeText.textContent);
+
+    if (likeBtn.classList.contains("clicked")) {
+        likeBtn.src = "./images/like_fill.svg"
+        likeText.textContent = currentLikeNum + 1;
+    } else {
+        likeBtn.src = "./images/like_empty.svg"
+        likeText.textContent = currentLikeNum - 1;
+    }
+});
+
+
+
+
 
 
