f8511d5f64c2ed98320f1cee828473fdb0253fcf
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-05-03 23:07:29 +0900

    feat: DOM 요소 선택

---
+++ front/HW/FEweek02_hw/week02_hw.js
@@ -0,0 +1,7 @@
+// DOM 요소 선택
+const likeBtn = document.querySelector("like-btn");
+const bookmarkBtn = document.querySelector("bookmark-btn");
+const moreBtn = document.querySelector("more-btn");
+const lessBtn = document.querySelector("less-btn");
+
+
