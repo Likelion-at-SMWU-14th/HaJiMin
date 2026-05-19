5616c8b75f42b187f71a08bfbcb53f606e6699c7
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-05-19 17:31:57 +0900

    feat: 검색 결과 없음 조건 추가

--- front/HW/FEweek03_hw/song.js
+++ front/HW/FEweek03_hw/song.js
@@ -4,21 +4,27 @@ const input = document.getElementById("input-text");
 
 // 노래방 리스트 가져오기
 async function fetchKaraoke(keyword) {
-    try {
-        const response = await fetch(`https://api.manana.kr/karaoke/singer/${keyword}.json`);
-        const songList = await response.json();
+  try {
+    const response = await fetch(
+      `https://api.manana.kr/karaoke/singer/${keyword}.json`,
+    );
+    const songList = await response.json();
 
-        const container = document.getElementById("songContainer");
-        
-        // 컨테이너 비우기
-        container.innerHTML = ""; 
+    const container = document.getElementById("songContainer");
 
-        songList.forEach((song) => {
-            const card = document.createElement("div");
-            card.className = "card";
+    // 컨테이너 비우기
+    container.innerHTML = "";
 
-            // 노래 카드 만들기
-            card.innerHTML = `
+    if (songList.length === 0) {
+      container.innerHTML = "<p>검색 결과가 없습니다.</p>";
+    }
+
+    songList.forEach((song) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      // 노래 카드 만들기
+      card.innerHTML = `
                 <h2 class="song-title">${song.title}</h2>
                 <p class="singer">${song.singer}</p>
                 <div class="song-detail-area">
@@ -33,26 +39,26 @@ async function fetchKaraoke(keyword) {
                 </div>
             `;
 
-            container.appendChild(card);
-        });
-    } catch (error) {
-        console.log("에러 발생: ", error);
-    }
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.log("에러 발생: ", error);
+  }
 }
 
 // 검색 함수
 function searchSong() {
-    btnSearch.addEventListener('click', () => {
-        if (input.value.trim() !== "") {
-            fetchKaraoke(input.value);
-        }
-    });
+  btnSearch.addEventListener("click", () => {
+    if (input.value.trim() !== "") {
+      fetchKaraoke(input.value);
+    }
+  });
 
-    input.addEventListener("keydown", (event) => {
-        if (event.key === "Enter" && input.value.trim() !== "") {
-            fetchKaraoke(input.value);
-        }
-    });
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" && input.value.trim() !== "") {
+      fetchKaraoke(input.value);
+    }
+  });
 }
 
 searchSong();
