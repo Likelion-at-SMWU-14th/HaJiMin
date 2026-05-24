873d5ad30b1d31be5319898ecd62764fe186d890
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-05-24 23:34:17 +0900

    feat: EuropeMap 유럽 지도 구현

--- front/HW/FEweek04_hw/src/components/EuropeMap.jsx
+++ front/HW/FEweek04_hw/src/components/EuropeMap.jsx
@@ -1,28 +1,28 @@
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Marker,
-} from "react-simple-maps";
-import styled from "styled-components";
+import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps'
+import styled from 'styled-components'
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json";
+const geoUrl = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'
 
 const MapContainer = styled.div`
   width: 100vw;
   height: 100vh;
   margin: 0;
   padding: 0;
-`;
+`
+
+const StyledMap = styled(ComposableMap)`
+  width: 100%;
+  height: 100%;
+`
 
 const EuropeMap = () => {
   return (
     <MapContainer>
-      <ComposableMap
-        projection="geoAzimuthalEqualArea"
+      <StyledMap
+        projection='geoAzimuthalEqualArea'
         projectionConfig={{
-          rotate: [-10, -52, 0], // 유럽이 중앙에 오도록 회전
-          scale: 700, // 확대 크기
+          rotate: [-10, -48, 0], // 유럽이 중앙에 오도록 회전
+          scale: 1700, // 확대 크기
         }}
       >
         <Geographies geography={geoUrl}>
@@ -32,16 +32,16 @@ const EuropeMap = () => {
                 key={geo.rsmKey}
                 geography={geo}
                 style={{
-                  default: { fill: "#D6D6DA", stroke: "#fff" }, // 기본 색
-                  hover: { fill: "#b0b0b0" }, // 호버 색
+                  default: { fill: '#D6D6DA', stroke: '#fff' }, // 기본 색
+                  hover: { fill: '#b0b0b0' }, // 호버 색
                 }}
               />
             ))
           }
         </Geographies>
-      </ComposableMap>
+      </StyledMap>
     </MapContainer>
-  );
-};
+  )
+}
 
-export default EuropeMap;
+export default EuropeMap
