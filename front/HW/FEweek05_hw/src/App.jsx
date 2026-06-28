ccaf0322e989a631c91c49fce48b701c81a45b16
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-06-28 19:21:16 +0900

    feat: 웹 기본 구조 작성

--- front/HW/FEweek05_hw/src/App.jsx
+++ front/HW/FEweek05_hw/src/App.jsx
@@ -1,7 +1,41 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <div>
+        {/* 상단 카테고리 */}
+        <div>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+        </div>
+
+        {/* 메인 제품 리스트 */}
+        <div>
+          <div>
+            <div>상품 이미지</div>
+            <div>상품 브랜드</div>
+            <div>상품 이름</div>
+            <div>가격</div>
+            <div>카테고리</div>
+          </div>
+          <div>
+            <div>상품 이미지</div>
+            <div>상품 브랜드</div>
+            <div>상품 이름</div>
+            <div>가격</div>
+            <div>카테고리</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
