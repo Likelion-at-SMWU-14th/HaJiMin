b107b5ff258c1f16c6e0f28f00ca9ac2a1317c73
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-07-12 21:05:52 +0900

    feat: 기본 Router 설정

--- front/HW/FEweek07_hw/src/App.jsx
+++ front/HW/FEweek07_hw/src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TodoPage from "./pages/TodoPage";
+import TodoDetailPage from "./pages/TodoDetailPage";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoPage />} />
+        <Route path="/detail/:todoId" element={<TodoDetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
