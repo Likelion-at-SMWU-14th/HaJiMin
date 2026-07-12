c0d3d663f5986484b0107d0cfe34ad7eb2cb1110
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-07-12 23:00:51 +0900

    feat: 새로운 todo 추가 로직

--- front/HW/FEweek07_hw/src/pages/TodoDetailPage.jsx
+++ front/HW/FEweek07_hw/src/pages/TodoDetailPage.jsx
@@ -1,15 +1,12 @@
-import ellipseBlank from "../assets/ellipse_blank.svg";
 import back from "../assets/back.svg";
+import TodoItem from "../components/TodoItem";
 
 function TodoDetailPage() {
   return (
     <>
       <div className="layout">
         <img className="btn-back" src={back} alt="뒤로가기" />
-        <div className="todo-item">
-          <img className="todo-status" src={ellipseBlank} alt="미완료" />
-          <span className="todo-detail">리액트 공부하기</span>
-        </div>
+        <TodoItem status={"blank"} detail={"리액트 공부하기"} />
         <textarea className="todo-memo" placeholder="메모를 입력하세요" />
         <div className="btn-group">
           <button className="btn-save">저장하기</button>
--- front/HW/FEweek07_hw/src/pages/TodoPage.jsx
+++ front/HW/FEweek07_hw/src/pages/TodoPage.jsx
@@ -1,13 +1,42 @@
 import TodoItem from "../components/TodoItem";
+import { useState } from "react";
 
 function TodoPage() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([
+    { id: 1, status: "blank", detail: "리액트 공부하기" },
+    { id: 2, status: "checked", detail: "장보기" },
+    { id: 3, status: "blank", detail: "멋사 FE 7주차 과제" },
+    { id: 4, status: "blank", detail: "방 청소" },
+  ]);
+
+  function addNewTodo() {
+    if (text.trim() === "") return;
+
+    const newTodoItem = {
+      id: Date.now(),
+      status: "blank",
+      detail: text,
+    };
+
+    setTodos([...todos, newTodoItem]);
+    setText("");
+  }
+
   return (
     <>
       <div className="layout">
         <p className="title">TODO</p>
         <div className="add-section">
-          <input className="add-input" placeholder="할 일을 입력하세요"></input>
-          <button className="btn-add">추가</button>
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="add-input"
+            placeholder="할 일을 입력하세요"
+          ></input>
+          <button onClick={() => addNewTodo()} className="btn-add">
+            추가
+          </button>
         </div>
         <div className="sort-section">
           <button className="btn btn-sort-all active">전체</button>
@@ -15,10 +44,9 @@ function TodoPage() {
           <button className="btn btn-sort-incomplete">미완료</button>
         </div>
         <div className="todo-list">
-          <TodoItem status={"blank"} detail={"리액트 공부하기"} />
-          <TodoItem status={"checked"} detail={"장보기"} />
-          <TodoItem status={"blank"} detail={"멋사 FE 7주차 과제"} />
-          <TodoItem status={"blank"} detail={"방 청소"} />
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} status={todo.status} detail={todo.detail} />
+          ))}
         </div>
       </div>
     </>
