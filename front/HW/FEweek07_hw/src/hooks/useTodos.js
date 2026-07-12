9b76d3c0cde5551813423f77f60d8c98bef55bd0
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-07-13 00:44:33 +0900

    feat: todo 저장 및 삭제 로직 구현

--- front/HW/FEweek07_hw/src/hooks/useTodos.js
+++ front/HW/FEweek07_hw/src/hooks/useTodos.js
@@ -3,8 +3,18 @@ import { useState } from "react";
 
 export function useTodos() {
   const [todos, setTodos] = useState([
-    { id: 1, status: "blank", detail: "리액트 공부하기", memo: "" },
-    { id: 2, status: "checked", detail: "장보기", memo: "" },
+    {
+      id: 1,
+      status: "blank",
+      detail: "리액트 공부하기",
+      memo: "멋사 세미나 내용 복습하기!",
+    },
+    {
+      id: 2,
+      status: "checked",
+      detail: "장보기",
+      memo: "바나나, 토마토, 빵, 치즈, 햄",
+    },
     { id: 3, status: "blank", detail: "멋사 FE 7주차 과제", memo: "" },
     { id: 4, status: "blank", detail: "방 청소", memo: "" },
   ]);
--- front/HW/FEweek07_hw/src/pages/TodoDetailPage.jsx
+++ front/HW/FEweek07_hw/src/pages/TodoDetailPage.jsx
@@ -7,13 +7,29 @@ function TodoDetailPage({ todos, setTodos, toggleStatus }) {
   const { todoId } = useParams();
   const navigate = useNavigate();
   const todo = todos.find((t) => t.id === Number(todoId));
-  const [newMemo, setNewMemo] = useState("");
+  const [newMemo, setNewMemo] = useState(todo?.memo ?? "");
+
+  if (!todo) {
+    return null; // "보여줄 게 없으면 그냥 아무것도 그리지 마"
+  }
+
+  function updateMemo() {
+    setTodos(
+      todos.map((t) => (t.id === todo.id ? { ...t, memo: newMemo } : t)),
+    );
+    navigate("/");
+  }
+
+  function deleteTodo() {
+    setTodos(todos.filter((t) => t.id !== todo.id));
+    navigate("/");
+  }
 
   return (
     <>
       <div className="layout">
         <img
-          onClick={navigate("/")}
+          onClick={() => navigate("/")}
           className="btn-back"
           src={back}
           alt="뒤로가기"
@@ -24,14 +40,18 @@ function TodoDetailPage({ todos, setTodos, toggleStatus }) {
           onToggle={() => toggleStatus(todo.id)}
         />
         <textarea
-          value={todo.memo}
+          value={newMemo}
           onChange={(e) => setNewMemo(e.target.value)}
           className="todo-memo"
           placeholder="메모를 입력하세요"
         />
         <div className="btn-group">
-          <button className="btn-save">저장하기</button>
-          <button className="btn-delete">삭제하기</button>
+          <button onClick={updateMemo} className="btn-save">
+            저장하기
+          </button>
+          <button onClick={deleteTodo} className="btn-delete">
+            삭제하기
+          </button>
         </div>
       </div>
     </>
