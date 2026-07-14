2c9d07cfb5cfc63c26b0621a5ed41a6379578d3e
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-07-14 16:58:08 +0900

    feat: 전체 게시글 조회 API 연동 완료

--- front/FEweek08/src/components/CommentList.jsx
+++ front/FEweek08/src/components/CommentList.jsx
@@ -1,9 +1,29 @@
-import { COMMENT_DATA } from "../constant/comment";
 import Comment from "./Comment";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const CommentList = () => {
-  const comments = COMMENT_DATA;
+  // 서버에서 API 응답을 받아와서 저장할 state 선언
+  const [comments, setComments] = useState([]);
+
+  const getComment = () => {
+    axios
+      .get("http://127.0.0.1:8000/entries/")
+      .then((res) => {
+        console.log(res);
+        setComments(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // 컴포넌트가 처음 마운트 됐을 때 API 요청 및 데이터 받아오기
+  useEffect(() => {
+    getComment();
+  }, []);
+
   return (
     <CommentWrapper>
       {comments.map((comment) => (
