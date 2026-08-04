fb8a3394308088aa1c1d80ad22501675d914ce66
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-04 16:00:58 +0900

    feat: ThemeProvider 적용

--- front/FEweek11/src/contexts/ThemeContext.jsx
+++ front/FEweek11/src/contexts/ThemeContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
 
+// Provider를 컴포넌트로 export -> App에서 따로 설정할 필요 없이 사용만 할 수 있음 !
 export const ThemeContextProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(false);
   const toggleTheme = () => {
