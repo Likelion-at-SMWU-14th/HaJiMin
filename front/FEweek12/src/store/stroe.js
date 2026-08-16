a6097925b44e6177301c41fcdfb8e73cb1f5ebd7
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-16 17:39:23 +0900

    feat: App.jsx 작성

--- front/FEweek12/src/store/stroe.js
+++ front/FEweek12/src/store/stroe.js
@@ -1,8 +1,9 @@
 import { create } from "zustand";
-import { devTools } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 
 const useTodoStore = create((set) => ({
   todos: [],
+
   addTodo: (rawTitle) => {
     const title = rawTitle.trim();
     if (!title) return;
@@ -10,11 +11,13 @@ const useTodoStore = create((set) => ({
       todos: [...state.todos, { id: Date.now(), title, completed: false }],
     }));
   },
+
   removeTodo: (id) =>
     set((state) => ({
       todos: state.todos.filter((todo) => todo.id !== id),
     })),
-  todoToggle: (id) =>
+
+  toggleTodo: (id) =>
     set((state) => ({
       todos: state.todos.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo,
