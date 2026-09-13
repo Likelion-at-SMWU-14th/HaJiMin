223033af1a1252d800509de8b1975c339622e186
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-09-13 23:47:18 +0900

    fix: 회원 정보 수정 시 마이페이지 정보 수정 반영되도록 수정

--- front/HW_FE-2/week2-2_hw/src/App.jsx
+++ front/HW_FE-2/week2-2_hw/src/App.jsx
@@ -1,11 +1,13 @@
 import SignupForm from "./components/SignupForm";
 import UpdateProfileForm from "./components/UpdateProfile";
+import MyPage from "./components/MyPage";
 
 function App() {
   return (
     <div>
       <SignupForm />
       <UpdateProfileForm />
+      <MyPage />
     </div>
   );
 }
--- front/HW_FE-2/week2-2_hw/src/api/user.js
+++ front/HW_FE-2/week2-2_hw/src/api/user.js
@@ -1,3 +1,9 @@
+let currentUser = {
+  userId: 1,
+  username: "하지민",
+  password: "1234",
+};
+
 // 회원가입
 export const signUp = async ({ username, password }) => {
   console.log("POST api/users");
@@ -7,17 +13,15 @@ export const signUp = async ({ username, password }) => {
 // 프로필 수정
 export const updateProfile = async ({ userId, ...updatedFields }) => {
   console.log(`PUT api/users/${userId}`, updatedFields);
+  currentUser = { ...currentUser, ...updatedFields };
+  return currentUser;
 };
 
 // 마이페이지 조회
 export const fetchMyPage = async (userId) => {
   console.log(`GET api/users/${userId}`);
 
-  return {
-    userId: 1,
-    username: "하지민",
-    password: "1234",
-  };
+  return currentUser;
 };
 
 // 회원 정보 삭제
