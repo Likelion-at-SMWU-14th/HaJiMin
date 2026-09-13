09f81e10a61d6261cbc51e828e82907accdebc15
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-09-13 22:24:09 +0900

    feat: posts CRUD API 함수 작성

---
+++ front/FE-2/week2-2/src/api/posts.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+// Create
+export const createPost = async (newPost) => {
+  const { data } = await axios.post(`${BASE_URL}/posts`, newPost);
+  return data;
+};
+// Read
+export const getPosts = async () => {
+  const { data } = await axios.get(`${BASE_URL}/posts`);
+  return data;
+};
+// Update
+export const updatePost = async (id, ...updateFields) => {
+  const { data } = await axios.put(`${BASE_URL}/posts/${id}`, updateFields);
+  return data;
+};
+// Delete
+export const deletePost = async (id) => {
+  const { data } = await axios.delete(`${BASE_URL}/posts/${id}`);
+  return data;
+};
